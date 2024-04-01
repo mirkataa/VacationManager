ea4a35a7dab67f655a75d7bdab67b0b7248a6ff6
Mirena Koleva <mire.koleva@gmail.com>
2024-04-01 19:14:09 +0300

    Add Roles Controller and Views

---
+++ VacationManager/VacationManager/Controllers/RolesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VacationManager.Data;
+using VacationManager.Models;
+
+namespace VacationManager.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RolesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Roles
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Roles.ToListAsync());
+        }
+
+        // GET: Roles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var roleModel = await _context.Roles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (roleModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(roleModel);
+        }
+
+        // GET: Roles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] RoleModel roleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(roleModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(roleModel);
+        }
+
+        // GET: Roles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var roleModel = await _context.Roles.FindAsync(id);
+            if (roleModel == null)
+            {
+                return NotFound();
+            }
+            return View(roleModel);
+        }
+
+        // POST: Roles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] RoleModel roleModel)
+        {
+            if (id != roleModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(roleModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoleModelExists(roleModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(roleModel);
+        }
+
+        // GET: Roles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var roleModel = await _context.Roles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (roleModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(roleModel);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var roleModel = await _context.Roles.FindAsync(id);
+            if (roleModel != null)
+            {
+                _context.Roles.Remove(roleModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoleModelExists(int id)
+        {
+            return _context.Roles.Any(e => e.Id == id);
+        }
+    }
+}
