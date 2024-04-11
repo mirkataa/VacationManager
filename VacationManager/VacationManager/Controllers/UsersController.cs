4a83627668ce11f3340ca8f7f744419b39a07edf
Mirena Koleva <mire.koleva@gmail.com>
2024-04-11 15:55:25 +0300

    Add Authorize

--- VacationManager/VacationManager/Controllers/UsersController.cs
+++ VacationManager/VacationManager/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,6 +12,7 @@ using VacationManager.Models;
 
 namespace VacationManager.Controllers
 {
+    [Authorize]
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _context;
